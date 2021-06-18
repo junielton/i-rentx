4d8272400d5fd5ca585c43b91518f6ac1f65df6d
junielton <junyellton@gmail.com>
2021-06-18 08:19:38 -0300

    create acounts rules

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,10 +1,10 @@
 import {container} from "tsyringe";
-import {UsersRepository} from "../../modules/accounts/repositories/implementations/UsersRepository";
-import {IUsersRepository} from "../../modules/accounts/repositories/IUsersRepository";
-import {ICategoryRepository} from "../../modules/cars/repositories/ICategoryRepository";
 import {CategoriesRepository} from "../../modules/cars/repositories/implementations/CategoriesRepository";
+import {ICategoryRepository} from "../../modules/cars/repositories/ICategoryRepository";
 import {SpecificationRepository} from "../../modules/cars/repositories/implementations/SpecificationRepository";
 import {ISpecificationRepository} from "../../modules/cars/repositories/ISpecificationRepository";
+import {UsersRepository} from "../../modules/accounts/repositories/implementations/UsersRepository";
+import {IUsersRepository} from "../../modules/accounts/repositories/IUsersRepository";
 
 container.registerSingleton<ICategoryRepository>("CategoriesRepository", CategoriesRepository);
 container.registerSingleton<ISpecificationRepository>("SpecificationRepository", SpecificationRepository);
