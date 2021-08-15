d3bf2b02b435ff4a0f439111a86491b156abcd5a
junielton <junyellton@gmail.com>
2021-08-15 19:11:05 -0300

    avatar upload

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -10,12 +10,14 @@ class UsersRepository implements IUsersRepository {
 		this.repository = getRepository(User);
 	}
 
-	async create({name, password, email, driver_license}: ICreateUserDTO): Promise<void> {
+	async create({name, password, email, driver_license, avatar, id}: ICreateUserDTO): Promise<void> {
 		const user = this.repository.create({
 			name,
 			password,
 			email,
 			driver_license,
+			avatar,
+			id,
 		});
 
 		await this.repository.save(user);
