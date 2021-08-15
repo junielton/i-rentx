228d8d0f1005aab6e903ad271b20b87ce13ee2cc
junielton <junyellton@gmail.com>
2021-08-15 16:06:41 -0300

    Add authemticantion to routes

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -3,8 +3,8 @@ import {User} from "../entities/User";
 
 interface IUsersRepository {
 	create(data: ICreateUserDTO): Promise<void>;
-
 	findByEmail(email: string): Promise<User>;
+	findByEmail(id: string): Promise<User>;
 }
 
 export {IUsersRepository};
